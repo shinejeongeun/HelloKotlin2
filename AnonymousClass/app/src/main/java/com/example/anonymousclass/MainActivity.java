a4b9ae6074ba012331592a20c9b0ea083ecee8e4
J-E shine <amy5549@naver.com>
2021-04-01 14:17:59 +0900

    Anonymous class 1 button

--- AnonymousClass/app/src/main/java/com/example/anonymousclass/MainActivity.java
+++ AnonymousClass/app/src/main/java/com/example/anonymousclass/MainActivity.java
@@ -7,24 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 Button mButton1;
 TextView mTextView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mButton1 = findViewById(R.id.button1);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener());
+        mButton1.setOnClickListener(this);
     }
 
-    private class MyOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             mTextView1.setText("You Clicked button 1");
 
         }
     }
-}
