fcbcd6bfca5739dddf5c9c2b99d8bbe7f2b55cdf
J-E shine <amy5549@naver.com>
2021-04-01 14:38:02 +0900

    Anonymous class for 2 buttons

--- AnonymousClass/app/src/main/java/com/example/anonymousclass/MainActivity.java
+++ AnonymousClass/app/src/main/java/com/example/anonymousclass/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-Button mButton1;
+Button mButton1, mButton2;
 TextView mTextView1;
 
     @Override
@@ -17,14 +17,21 @@ TextView mTextView1;
         setContentView(R.layout.activity_main);
 
         mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
     }
 
         @Override
-        public void onClick(View view) {
-            mTextView1.setText("You Clicked button 1");
+        public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button1 : mTextView1.setText("You clicked button 1"); break;
+            case R.id.button2: mTextView1.setText("You clicked button 2"); break;
+
+            default: break;
+        }
 
         }
     }
