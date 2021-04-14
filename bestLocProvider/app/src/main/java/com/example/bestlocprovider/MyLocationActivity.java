7e0ebb0ebd31615a0c4fb35bffb2a6416152c64a
J-E shine <amy5549@naver.com>
2021-04-14 11:36:48 +0900

    complete onresume

---
+++ bestLocProvider/app/src/main/java/com/example/bestlocprovider/MyLocationActivity.java
@@ -0,0 +1,77 @@
+package com.example.bestlocprovider;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MyLocationActivity extends AppCompatActivity {
+    LocationManager locManager;
+    LocationListener locListener;
+    TextView myLocField;
+    Criteria criteria;
+    String bestProvName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myLocField = findViewById(R.id.myLocationfield);
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        bestProvName = locManager.getBestProvider(criteria, true);
+        // myLocField.setText("Best Loc. provider: " + bestProvName);
+
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                myLocField.setText(
+                        "Latitude:" + location.getLongitude()
+                                + "\nLongitude:" + location.getLongitude()
+                                + "\nAltitude:" + location.getLongitude());
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(MyLocationActivity.this,
+                    "First enable LoCATION ACCESS in Settings.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+        //locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locListener);
+        //locManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, locListener);
+    }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            locManager.removeUpdates(locListener);
+        }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+}
