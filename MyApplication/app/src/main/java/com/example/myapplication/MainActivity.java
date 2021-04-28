cb968f2974716a0cf43bbe39ac4c64982153e214
J-E shine <amy5549@naver.com>
2021-04-28 09:55:48 +0900

    Hello Kotlin initial commit

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,22 +2,44 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.CheckBox;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    TextView t2, t3, t4;
-    CheckBox c1, c2, c3;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Intent intent;
+    Button Fbutton, Rbutton, Sbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("밥");
 
-        t2 = findViewById(R.id.textView2);
-        t3 = findViewById(R.id.textView3);
-        t4 = findViewById(R.id.textView4);
+
+        Fbutton = findViewById(R.id.button1);
+        Fbutton.setOnClickListener(this);
+        Rbutton = findViewById(R.id.button2);
+        Rbutton.setOnClickListener(this);
+        Sbutton = findViewById(R.id.button3);
+        Sbutton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                intent = new Intent(this, FoodActivity.class);
+            case R.id.button2:
+                intent = new Intent(this, RunActivity.class);
+            case R.id.button3:
+                intent = new Intent(this, SnackActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
+
