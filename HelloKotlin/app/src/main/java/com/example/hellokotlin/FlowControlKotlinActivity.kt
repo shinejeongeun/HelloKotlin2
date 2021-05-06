17d15e806c8efa9f0aaee9ce1277a0056c7b1264
J-E shine <amy5549@naver.com>
2021-05-06 17:20:10 +0900

    Flow Control in kotlin

--- HelloKotlin/app/src/main/java/com/example/hellokotlin/FlowControlKotlinActivity.kt
+++ HelloKotlin/app/src/main/java/com/example/hellokotlin/FlowControlKotlinActivity.kt
@@ -2,10 +2,35 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class FlowControlKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_flowcontrol)
+
+        val btnLd = findViewById<Button>(R.id.btnLD)
+        val fieldNumber = findViewById<EditText>(R.id.fieldNumber)
+
+        btnLd.setOnClickListener {
+            val number = fieldNumber.text.toString().toInt()
+
+            if(number % 2 == 0) {
+                Toast.makeText(applicationContext, "${number} 은(는) 2의 배수", Toast.LENGTH_SHORT).show()
+            }
+            else if(number % 3 == 0) {
+                Toast.makeText(applicationContext, "${number} 은(는) 3의 배수", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "${number} 은(는) else", Toast.LENGTH_LONG).show()
+            }
+            when(number) {
+                4,9 -> btnLd.text = "실행 for $ number}"
+                else -> btnLd.text = "실행 for else"
+            }
+
+        }
     }
 }
